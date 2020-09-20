891c44d5a3a7bba4f5e89f9f18efad9e454dd4f0
Pawan Deshpande <Pawan.Deshpande@ril.com>
2020-09-20 20:28:30 +0530

    first commit

---
+++ app/src/main/java/com/tutorialsbuzz/recyclerview/TabFragments/ItemViewHolder.java
@@ -0,0 +1,31 @@
+package com.tutorialsbuzz.recyclerview.TabFragments;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tutorialsbuzz.recyclerview.R;
+
+/**
+ * Created by iFocus on 29-10-2015.
+ */
+public class ItemViewHolder extends RecyclerView.ViewHolder {
+
+    private TextView name_TextView;
+    private TextView iso_TextView;
+
+    public ItemViewHolder(View itemView) {
+        super(itemView);
+        name_TextView = (TextView) itemView.findViewById(R.id.country_name);
+        iso_TextView = (TextView) itemView.findViewById(R.id.country_iso);
+
+    }
+
+    public void bind(CountryModel countryModel) {
+        name_TextView.setText(countryModel.getName());
+        iso_TextView.setText(countryModel.getisoCode());
+
+    }
+
+}
